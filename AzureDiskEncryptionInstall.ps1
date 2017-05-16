0729987e5746a85f3d5d5d64b8147f1fc3329a95
Jordan Helton <jhelton@teklinks.com>
2017-05-16 13:16:39 -0400

    InitialCommit

---
+++ AzureDiskEncryptionInstall.ps1
@@ -0,0 +1,10 @@
+﻿$vmName = 'DEMO-Server01' 
+$resourceGroupName = 'EncryptionDemo' 
+$aadClientID = '' 
+$aadClientSecret = '' 
+$diskEncryptionKeyVaultUrl = '' 
+$keyVaultResourceId = ''
+$keyEncryptionKeyUrl = '' 
+$keyEncryptionKeyVaultID = ''
+ 
+Set-AzureRmVMDiskEncryptionExtension -ResourceGroupName $resourceGroupName -VMName $vmName -AadClientID $aadClientID -AadClientSecret $aadClientSecret -DiskEncryptionKeyVaultUrl $diskEncryptionKeyVaultUrl -DiskEncryptionKeyVaultId $keyVaultResourceId -KeyEncryptionKeyURL $keyencryptionkeyurl -KeyEncryptionKeyVaultId $keyEncryptionKeyVaultID -VolumeType all -skipVMbackup
